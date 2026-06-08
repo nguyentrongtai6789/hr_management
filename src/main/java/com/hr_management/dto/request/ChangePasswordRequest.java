8fd40a109d14a51dde8c66b25eabb43a2c5251e9
trongtai <trongtai.nguyen789@gmail.com>
2026-06-08 20:49:15 +0700

    update

---
+++ src/main/java/com/hr_management/dto/request/ChangePasswordRequest.java
@@ -0,0 +1,19 @@
+package com.hr_management.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ChangePasswordRequest {
+    @NotBlank(message = "Mật khẩu hiện tại không được để trống")
+    private String currentPassword;
+
+    @NotBlank(message = "Mật khẩu mới không được để trống")
+    private String newPassword;
+}
